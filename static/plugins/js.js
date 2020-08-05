6f04b0945f2f077303e1e9fa78bd0c187372793b
wwping <iswwping@163.com>
2020-08-05 19:25:11 +0800

    update plugin

--- static/plugins/js.js
+++ static/plugins/js.js
@@ -1,40 +1,40 @@
 /*
  * @Author: your name
  * @Date: 2020-07-27 20:38:40
- * @LastEditTime: 2020-07-29 23:11:22
+ * @LastEditTime: 2020-08-05 10:28:43
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \undefinedd:\Desktop\plugins\js.js
- */ 
-function getParam(name) {
+ */
+function getParam (name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
     var r = window.location.search.substr(1).match(reg);
-    if ( r != null ){
+    if (r != null) {
         return decodeURI(r[2]);
-    }else{
+    } else {
         return null;
     }
 }
 
-function addz(x){
-    if(x<10){
-        x="0"+x;
+function addz (x) {
+    if (x < 10) {
+        x = "0" + x;
     }
     return x;
 }
 
-String.prototype.hex2rgb = function(){
+String.prototype.hex2rgb = function () {
     var color = this.toLowerCase();
     var pattern = /^#([0-9|a-f]{3}|[0-9|a-f]{6})$/;
-    if(color && pattern.test(color)) {
-        if(color.length == 4) {
+    if (color && pattern.test(color)) {
+        if (color.length == 4) {
             // 将三位转换为六位
-              color = '#' + color[1] + color[1] + color[2] + color[2] + color[3] + color[3];
+            color = '#' + color[1] + color[1] + color[2] + color[2] + color[3] + color[3];
         }
         //处理六位的颜色值
         var colorNew = [];
-        for (var i=1; i<7; i+=2) {
-            colorNew.push(parseInt("0x"+color.slice(i, i+2)));  
+        for (var i = 1; i < 7; i += 2) {
+            colorNew.push(parseInt("0x" + color.slice(i, i + 2)));
         }
         return colorNew;
     }
@@ -42,16 +42,20 @@ String.prototype.hex2rgb = function(){
 };
 
 
-function randomNum(minNum,maxNum){ 
-    switch(arguments.length){ 
-        case 1: 
-            return parseInt(Math.random()*minNum+1,10); 
-        break; 
-        case 2: 
-            return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
-        break; 
-            default: 
-                return 0; 
-            break; 
-    } 
-} 
+function randomNum (minNum, maxNum) {
+    switch (arguments.length) {
+        case 1:
+            return parseInt(Math.random() * minNum + 1, 10);
+            break;
+        case 2:
+            return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10);
+            break;
+        default:
+            return 0;
+            break;
+    }
+}
+
+Number.prototype.toMinuteStr = function () {
+    return `${addz(Math.floor((this / 60 % 60)))}:${addz(Math.floor((this % 60)))}`
+}
